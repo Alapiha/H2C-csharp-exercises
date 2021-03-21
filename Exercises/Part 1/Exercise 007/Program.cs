f69e0ecb8827aef179f49411c6428bafbae63cdb
Alapiha <kaisa.alapiha@gmail.com>
2021-03-21 23:27:50 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 007/Program.cs
+++ Exercises/Part 1/Exercise 007/Program.cs
@@ -7,7 +7,9 @@ namespace exercise_07
         public static void Main(string[] args)
         {
             Console.WriteLine("Give input!");
-            // Write your code here:
+            string input = Console.ReadLine();
+            Console.WriteLine(input + "\n" + input + "\n" + input);
+            Console.ReadKey();
             
         }
     }
