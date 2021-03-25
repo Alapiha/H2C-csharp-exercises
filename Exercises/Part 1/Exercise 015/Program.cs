b2cf2a1574a7f5417c2310baf07a246cbad44ce6
Alapiha <kaisa.alapiha@gmail.com>
2021-03-25 22:11:13 +0200

    25.3.2021

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -6,9 +6,20 @@ namespace exercise_15
   {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Give a string:");
+        string message = Console.ReadLine();
+        Console.WriteLine("Give an integer:");
+        int intValue = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give a double:");
+        double doubleValue = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Give a boolean:");
+        bool truth = System.Convert.ToBoolean(Console.ReadLine());
 
-      // Write your code here:
-
+        Console.WriteLine("Your string: " + message);
+        Console.WriteLine("Your integer: " + intValue);
+        Console.WriteLine("Your double: " + doubleValue);
+        Console.WriteLine("Your boolean: " + truth);
+        Console.ReadKey();
     }
   }
 }
