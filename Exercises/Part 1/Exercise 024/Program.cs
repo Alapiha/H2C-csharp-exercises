15ac4eab4fe02aef37f7b33b7d877c124628263f
Alapiha <kaisa.alapiha@gmail.com>
2021-03-26 15:42:52 +0200

    26.3.2021

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -6,9 +6,13 @@ namespace exercise_24
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
-      
+        Console.WriteLine("Your speed:");
+        int speed = Convert.ToInt32(Console.ReadLine());
+        if (speed > 120)
+        { 
+            Console.WriteLine("Speeding!");    
+        }
+        Console.ReadKey();
     }
   }
 }
