38c9affe2a47d78b4e7b5e20e1a487175d694558
Alapiha <kaisa.alapiha@gmail.com>
2021-03-26 14:08:03 +0200

    26.3.2021

--- Exercises/Part 1/Exercise 020/Program.cs
+++ Exercises/Part 1/Exercise 020/Program.cs
@@ -6,8 +6,12 @@ namespace exercise_20
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        Console.WriteLine("Give the first number!");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number!");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(first + " * " + second + " = " + (first * second));
+        Console.ReadKey(); 
 
     }
   }
