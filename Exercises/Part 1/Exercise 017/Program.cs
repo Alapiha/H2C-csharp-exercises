924e6dd96e147c57fd29c2e1c80bb29647a7926c
Alapiha <kaisa.alapiha@gmail.com>
2021-03-26 13:35:50 +0200

    26.3.2021

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -6,8 +6,12 @@ namespace exercise_17
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        Console.WriteLine("Give the first number!");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number!");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum is " + (first + second));
+        Console.ReadKey(); 
 
     }
   }
