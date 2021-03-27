749034aab7b580ae6f6658641e077a9c33308b4b
Alapiha <kaisa.alapiha@gmail.com>
2021-03-27 21:13:07 +0200

    27.3.2021

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -6,8 +6,26 @@ namespace exercise_40
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        int amount = 0; 
+        int sum = 0;
+        while (true) 
+        {
+            Console.WriteLine("Give a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number == 0) 
+            {
+                break;
+            } 
+            if (number != 0)
+            {
+                sum = sum + number;
+                amount = amount + 1;
+            }
+        }
+        Console.WriteLine("Total sum of numbers: " + sum);
+        Console.WriteLine("Total amount of numbers: " + amount);
+        Console.ReadKey();
+      
     }
   }
 }
