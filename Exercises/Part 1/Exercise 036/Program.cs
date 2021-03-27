eb8130fe0e688ecf81165b2c1057cf56d36de29c
Alapiha <kaisa.alapiha@gmail.com>
2021-03-27 19:32:36 +0200

    27.3.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,7 +6,22 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+        while (true) 
+        {
+            Console.WriteLine("Give a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number == 0) 
+            {
+                break;
+            } 
+            if (number < 0)
+            {
+                    Console.WriteLine("That is negative");
+                    continue;
+            }
+            Console.WriteLine(number * number);
+        }
+        Console.ReadKey();
 
     }
   }
