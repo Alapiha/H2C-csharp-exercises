12c90c3a6e84d6425764cad8df4bf22c69d5e950
Alapiha <kaisa.alapiha@gmail.com>
2021-03-27 23:38:02 +0200

    27.3.2021

--- Exercises/Part 2/Exercise 041/Program.cs
+++ Exercises/Part 2/Exercise 041/Program.cs
@@ -6,9 +6,11 @@ namespace exercise_41
   {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Math.Pow(number, 2));
+        Console.ReadKey();
 
-      // Write your code here:
-      
     }
   }
 }
