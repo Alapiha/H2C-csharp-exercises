8185a34e3e1a063ebf309147ad523efee969a5c0
Alapiha <kaisa.alapiha@gmail.com>
2021-03-29 16:32:29 +0300

    29.3.2021

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -6,8 +6,12 @@ namespace exercise_42
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        Console.WriteLine("Give the first number:");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number:");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Math.Sqrt(first + second));
+        Console.ReadKey();
     
     }
   }
