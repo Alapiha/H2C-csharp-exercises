88345d4015961a6841102434d4477f9a2727790b
Alapiha <kaisa.alapiha@gmail.com>
2021-04-04 22:55:34 +0300

    4.4.2021

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,8 +6,44 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+        double sum = 0;
+        int amount = 0;
+        double average = 0;
+        int even = 0;
+        int odd = 0;
 
-      // Write your code here:
+        while (true) 
+        {
+            Console.WriteLine("Write numbers:");
+            int number = Convert.ToInt32(Console.ReadLine());
+ 
+            if (number == -1)
+            {
+                Console.WriteLine("Thx! Bye!");
+                Console.WriteLine("Sum: " + sum);
+                Console.WriteLine("Numbers: " + amount);
+                Console.WriteLine("Average: " + average);
+                Console.WriteLine("Even: " + even);
+                Console.WriteLine("Odd: " + odd);
+                break;
+            }
+            else
+            {
+                sum = sum + number; 
+                amount++;
+                average = sum / amount;
+                
+                if ((number % 2) == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+        }
+        Console.ReadKey();
  
     }
   }
