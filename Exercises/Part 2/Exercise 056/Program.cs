19ea06e49d582975dd87f2c86bd1e7f7d92e3c3d
Alapiha <kaisa.alapiha@gmail.com>
2021-04-05 21:49:01 +0300

    5.4.2021

--- Exercises/Part 2/Exercise 056/Program.cs
+++ Exercises/Part 2/Exercise 056/Program.cs
@@ -6,10 +6,14 @@ namespace exercise_56
   {
     public static void Main(String[] args)
     {
-
+        string words = Word(); 
+        Console.WriteLine(words);
+        Console.ReadKey();
     }
 
-    // Write your method here:
-
+    public static string Word()
+    {
+        return "Hello World!";
+    }
   }
 }
