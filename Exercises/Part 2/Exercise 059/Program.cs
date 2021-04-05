9244b0e747ad5dea8406734c7fb9c383cc566bc5
Alapiha <kaisa.alapiha@gmail.com>
2021-04-05 23:02:21 +0300

    5.4.2021

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -6,11 +6,32 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
-      Console.WriteLine("Greatest: " + answer);
+        int answer = Greatest(2, 7, 3);
+        Console.WriteLine("Greatest: " + answer);
+        Console.ReadKey();
     }
 
-    // Write your method here:
-
+    public static int Greatest(int number1, int number2, int number3)
+    {
+        if (number1 > number2)
+        {
+            if (number1 > number3)
+            {
+                return number1;
+            }
+            else
+            {
+                return number3;
+            }
+        }
+        else if (number2 > number3)
+        {
+            return number2;
+        }
+        else
+        {
+            return number3;
+        }
+    }
   }
 }
