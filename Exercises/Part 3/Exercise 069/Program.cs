e7ccc341cd031a7d732c9f04e718cb6bced413e2
Alapiha <kaisa.alapiha@gmail.com>
2021-04-12 17:11:19 +0300

    12.4.2021

--- Exercises/Part 3/Exercise 069/Program.cs
+++ Exercises/Part 3/Exercise 069/Program.cs
@@ -7,17 +7,34 @@ namespace exercise_69
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        List<int> list = new List<int>();
+        while (true)
         {
-          break;
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == -1)
+            {
+                break;
+            }
+            list.Add(input);
         }
-        list.Add(input);
-      }
+        Console.WriteLine("From where?");
+        int fromWhere = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Where to?");
+        int whereTo = Convert.ToInt32(Console.ReadLine());
 
+        int i = 0;
+        while (i < list.Count) 
+        {
+            if (fromWhere <= list[i])
+            { 
+                if (whereTo >= list[i])
+                {
+                    Console.WriteLine(list[i]);
+                }
+            }
+            i++;
+        } 
+        Console.ReadKey();
     }
   }
 }
