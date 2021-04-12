da891d1f3a07dbc377e0c024e5b97b036c529e86
Alapiha <kaisa.alapiha@gmail.com>
2021-04-12 21:19:23 +0300

    12.4.2021

--- Exercises/Part 3/Exercise 070/Program.cs
+++ Exercises/Part 3/Exercise 070/Program.cs
@@ -7,16 +7,29 @@ namespace exercise_70
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        List<int> list = new List<int>();
+        while (true)
         {
-          break;
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == -1)
+            {
+                break;
+            }
+            list.Add(input);
         }
-        list.Add(input);
-      }
+        int greatest = list[0];
+
+        for(int i = 0; i < list.Count; i++) 
+        {
+            int number = list[i];
+            if (greatest < number) 
+            {
+                greatest = number;
+            }
+        }
+
+        Console.WriteLine("The greatest number: " + greatest);
+        Console.ReadKey();
     }
   }
 }
