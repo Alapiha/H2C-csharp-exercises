146c84a6ce14df44309fd92135d6176720615e08
Alapiha <kaisa.alapiha@gmail.com>
2021-04-16 23:45:35 +0300

    16.4.2021

--- Exercises/Part 3/Exercise 071/Program.cs
+++ Exercises/Part 3/Exercise 071/Program.cs
@@ -7,17 +7,28 @@ namespace exercise_71
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        List<int> list = new List<int>();
+        while (true)
         {
-          break;
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == -1)
+            {
+                break;
+            }
+            list.Add(input);
         }
-        list.Add(input);
-      }
+        Console.WriteLine("What number are you searching?");
+        int number = Convert.ToInt32(Console.ReadLine());
 
+        for (int i = 0; i < list.Count; i++) 
+        {
+            if (number == list[i])
+            {         
+                Console.WriteLine(number + " is at index " + i);     
+            }
+            
+        }
+        Console.ReadKey();
     }
   }
 }
