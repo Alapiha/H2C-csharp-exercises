3fd15e3d961e84af9827161acaa80df6c75e7dd9
Alapiha <kaisa.alapiha@gmail.com>
2021-04-20 00:45:01 +0300

    20.4.2021

--- Exercises/Part 3/Exercise 086/Program.cs
+++ Exercises/Part 3/Exercise 086/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
-
+        while (true)
+        { 
+            string text = Console.ReadLine();
+            if (text == "")
+            {
+                break;
+            }
+            string[] pieces = text.Split(' ');
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                Console.WriteLine(pieces[i]);
+            }
+        }
+        Console.ReadKey();
 
     }
   }
