9afcddf6433d1ce3b978126939c2916bfe77ef0a
Alapiha <kaisa.alapiha@gmail.com>
2021-04-29 20:05:11 +0300

    29.4.2021

--- Exercises/Part 3/Exercise 090/Program.cs
+++ Exercises/Part 3/Exercise 090/Program.cs
@@ -7,6 +7,26 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
+        
+        int x = 0;
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "")
+            {
+                break;
+            }
+            string[] parts = input.Split(',');
+            
+            int age = Convert.ToInt32(parts[1]);
+            if (x < age)
+            {
+                x = age;
+            }
+
+        }
+        Console.WriteLine("Age of the oldest: " + x);
+        Console.ReadKey();
 
 
     }
