16a9ebabd3e8da0d2faf5bdc806b943a20e9a08d
Alapiha <kaisa.alapiha@gmail.com>
2021-04-29 22:05:11 +0300

    29.4.2021

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,7 +7,33 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
-
+        string name = "longest";   
+        int l = 0;
+        int x = 2020;
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "")
+            {
+                break;
+            }
+            string[] parts = input.Split(',');
+            
+            int year = Convert.ToInt32(parts[1]);
+            if (x > year)
+            {
+                x = year;       
+            }
+            if (l < parts[0].Length)
+            {
+                l = parts[0].Length;
+                name = parts[0];
+            }
+        }
+        
+        Console.WriteLine("Longest name: " + name);
+        Console.WriteLine("Highest age: " + (2020 - x));
+        Console.ReadKey();
     }
   }
 }
